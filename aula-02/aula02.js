c332527ebbe0bda0173b8097c087c95ce7f0fb10
Lucas Paim <lucaspaim924@gmail.com>
2025-04-10 11:37:11 -0300

    aula 02

---
+++ aula-02/aula02.js
@@ -0,0 +1,12 @@
+//Teste 1: Diferentes formas de imprimir no terminal
+let nome="Bruno"  //declara a variável
+console.log("CFB Cursos") //Imprimi uma string
+console.log(nome) //Imprimi uma variável que é puxada pelo let
+console.log("Nome: " + nome) //Concatena uma string e uma variável
+
+//Teste 2: Usando o modo Estrito (Strict)
+"use strict" //Modo estrito
+nome="Bruno"  // Vai dar erro
+console.log("CFB Cursos")
+console.log(nome)
+console.log("Nome: " + nome)
