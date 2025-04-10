3d61bf52d1d5aeda8e7c115d42bdbf12b91f2018
Lucas Paim <lucaspaim924@gmail.com>
2025-04-10 11:21:00 -0300

    Aula 01

---
+++ aula1.js
@@ -0,0 +1,14 @@
+//Teste 1: Printar em um terminal usamos o console.log
+console.log("Olá Mundo!")
+
+//Teste 2: como comentar uma LINHA
+//console.log("Essa linha está comentada")
+console.log("Olá Mundo!")
+
+//Teste 3: Comentário de bloco de código
+/*
+console.log("Essa linha está comentada")
+console.log("Olá Mundo!")
+*/
+console.log("Fim")
+
