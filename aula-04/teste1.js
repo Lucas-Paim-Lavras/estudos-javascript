6711cb9caa51ec823ec463cf16b7af7c2dee5d8c
Lucas Paim <lucaspaim924@gmail.com>
2025-04-21 20:18:04 -0300

    Aprendendo sobre operadores em javascript - não finalizado

---
+++ aula-04/teste1.js
@@ -0,0 +1,8 @@
+let num1=10, num2=10 //preciso declarar as duas separadamente só que na mesma linha
+
+num1=num2=num3=10 //posso já declarar tudo assim de uma vez
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
