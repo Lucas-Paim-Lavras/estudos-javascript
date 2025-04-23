7b04ae58823b3d98d630717e4fa394854808379d
Lucas Paim <lucaspaim924@gmail.com>
2025-04-23 20:56:05 -0300

    aula 06

---
+++ aula-05-OperadoresRelacionais/teste1.js
@@ -0,0 +1,27 @@
+/*
+    == igual (com coerção de tipo)
+
+    === estritamente igual (sem coerção)
+
+    != diferente
+
+    !== estritamente diferente
+
+    > maior que
+
+    < menor que
+
+    >= maior ou igual
+
+    <= menor ou igual
+*/
+
+let num1 = 8
+let num2 = 4
+let num3 = 8
+
+console.log (num1 > num3)
+console.log (num1 >= 3)
+console.log (!(num1 == num3)) //o ! (not) inverte a resposta do operador relacional. Nesse caso aqui, teríamos que num1 é igual ao num3 mas o ! antes faz com que retorne False
+console.log (num1 != num3)  //diferença normal
+
---
+++ aula-06/teste1.js
@@ -0,0 +1,8 @@
+let n1=1
+let n2="1"
+
+let v1={nome:"Lucas"}
+let v2={nome:"Lucas"}
+
+console.log(n1===n2)
+console.log(v1===v2)
