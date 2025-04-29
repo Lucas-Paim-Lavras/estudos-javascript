2e2a332af3d363b441cc502f718188f5c1cd50c6
Lucas Paim <lucaspaim924@gmail.com>
2025-04-29 11:02:46 -0300

    Aula 08 - operadores bitwise

---
+++ aula-08-operadoresBitwise/teste1.js
@@ -0,0 +1,8 @@
+let n1=10
+let n2=11
+
+let resultado = n1 & n2  //Bitwise AND que ensina a tabela. Bsicamente verifica se há equivalência entre os números. Se houver, ele retorna 1, se não houver, ele retorna 0. Ver a tabela de verdade.
+
+
+console.log(resultado)
+
---
+++ aula-08-operadoresBitwise/teste2.js
@@ -0,0 +1,8 @@
+let n1=10
+let n2=11
+
+let resultado = n1 | n2  //Bitwise OR que ensina a tabela. Bsicamente verifica se há equivalência entre os números. Ele apenas retorna 1 se tivermos 1 na comparação se não houver (apenas 0) ele retorna 0. Ver a tabela de verdade.
+//n1 e n2 são convertidos para binário e depois comparados. O resultado é convertido de volta para decimal.
+
+
+console.log(resultado)
