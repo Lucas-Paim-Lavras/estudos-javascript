04dc474fbbf249d4c354c6d37773b22d80e9bc1c
Lucas Paim <lucaspaim924@gmail.com>
2025-05-07 11:40:25 -0300

    aula 10 - Operador Ternário

---
+++ aula-10- operadorTernario/teste1.js
@@ -0,0 +1,11 @@
+//Como verificar se um número é par ou ímpar
+
+let num=11
+
+let res = num%2
+
+if(res==0){
+    console.log("Par")
+}else{
+    console.log("Ímpar")
+}
---
+++ aula-10- operadorTernario/teste2.js
@@ -0,0 +1,18 @@
+let num=13
+res = (num%2 ? "Ímpar" : "Par")  //O primeiro resultado é quando o resto da divisão por dois é 
+console.log(res)
+
+//0 = False
+//1 = True
+
+// teste lógico ? se  verdadeiro : se falso  -- Essa é a sintaxe
+
+let num1=13
+let num2=5
+res1 = (num1 < num2 ? "Verdadeiro" : "Falso")  //O primeiro resultado é quando o resto da divisão por dois é 
+console.log(res1)
+
+//Quando os ternários sao usados
+let st="A"    //se ver o status dobanco de dados de alguma pessoa se o usuário é ativo ou inativo ao invés de aparecer na tela "I" ou "A". Podemos colocar de uma forma mais formalizada como "Inativo" ou "Ativo"
+res2 =(st =="A" ? "Ativo" : "Inativo")
+console.log(res2)
