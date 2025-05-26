81c41f59346219c61d9401c7f55464cec86be5bc
Lucas Paim <lucaspaim924@gmail.com>
2025-05-26 12:03:20 -0300

    aula 18 - Do while

---
+++ aula-18-doWhile/teste1.js
@@ -0,0 +1,16 @@
+//no do While eu tenho a garantia de executar o programa, pelo menos uma vez 
+let numero=10
+
+while(numero<10){
+    console.log("CFB Cursos")
+    numero++
+}
+console.log("Fim do programa")
+
+
+
+// do{
+//     console.log("CFB Cursos")
+//     n++
+// }while(n<10)
+// console.log("Fim do programa")
