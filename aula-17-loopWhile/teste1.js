1c7ffecb344435e2d2d3178c34aa47c129ca0833
Lucas Paim <lucaspaim924@gmail.com>
2025-05-26 11:47:49 -0300

    aula 17 - loop while

---
+++ aula-17-loopWhile/teste1.js
@@ -0,0 +1,12 @@
+
+
+let n = 0
+// while(n<=10){
+//     console.log(n)
+//     n++
+// }
+
+while(true){
+    console.log(n)
+    n++
+}              //Caso de loop infinito
---
+++ aula-17-loopWhile/teste2.js
@@ -0,0 +1,10 @@
+let numero = 10
+let fatorial = 1
+
+while (numero >= 1){
+    fatorial*=numero
+    numero--
+    console.log(numero + " -> " + fatorial)
+}
+
+// console.log(fatorial)
