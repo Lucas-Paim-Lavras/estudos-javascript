bb05fe54dc34158d7cba2feb7d7fe74a944881fb
Lucas Paim <lucaspaim924@gmail.com>
2025-05-27 12:05:01 -0300

    Funções em javacsript

---
+++ aula-20-funcoes/teste1.js
@@ -0,0 +1,34 @@
+//Ex1
+function nome(){      //declaração da função
+    console.log("CFB Cursos")
+}
+//nome()   //Chamada da função
+for (let i = 0; i < 10; i++){
+    nome()      //chamei a função dentro de um loop for
+}
+
+
+//Ex2
+function soma_2(){      //declaração da função
+    let numero1 = 10
+    let numero2 = 2
+    let soma = numero1 + numero2
+    console.log(soma)
+}
+
+for (let i = 0; i < 10; i++){
+    soma_2()      //chamei a função dentro de um loop for
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
