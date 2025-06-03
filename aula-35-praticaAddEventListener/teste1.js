38083baa13ddcecfdb6cdaddaa399353afb765a4
Lucas Paim <lucaspaim924@gmail.com>
2025-06-03 10:26:47 -0300

    correções co m novo botão de voltar

--- aula-35-praticaAddEventListener/teste1.js
+++ aula-35-praticaAddEventListener/teste1.js
@@ -1,6 +1,7 @@
 const caixa1 = document.querySelector("#caixa1");
 const caixa2 = document.querySelector("#caixa2");
 const btn = document.querySelector("#btn_copiar");
+const btn2 = document.querySelector("#btn_voltar");
 const todosCursos = [...document.querySelectorAll(".cursos")];
 
 todosCursos.map((el) => {
@@ -12,11 +13,17 @@ todosCursos.map((el) => {
 
 btn.addEventListener("click", () => {
     const cursos = [...document.querySelectorAll(".cursos")];
-
-    cursos.forEach((curso) => {
+    cursos.map((curso) => {
         if (curso.classList.contains("selecionado") && caixa1.contains(curso)) {
             caixa2.appendChild(curso); // Vai da caixa1 pra caixa2
-        } else if (!curso.classList.contains("selecionado") && caixa2.contains(curso)) {
+        }
+    });
+});
+
+btn2.addEventListener("click", () => {
+    const cursos = [...document.querySelectorAll(".cursos")];
+    cursos.map((curso) => {
+        if (curso.classList.contains("selecionado") && caixa2.contains(curso)) {
             caixa1.appendChild(curso); // Volta pra caixa1 se desmarcado
         }
     });
