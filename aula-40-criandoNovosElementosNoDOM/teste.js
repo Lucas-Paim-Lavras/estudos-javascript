b8c895fb17bcadf0d74f4c3bf15a19d2c20893a5
Lucas Paim <lucaspaim924@gmail.com>
2025-06-04 19:43:18 -0300

    Estudos

---
+++ aula-40-criandoNovosElementosNoDOM/teste.js
@@ -0,0 +1,6 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".cursos")]
+const c1_2 = document.querySelector("#c1_2")
+
+const novoElemento = document.createElement("div")
+caixa
