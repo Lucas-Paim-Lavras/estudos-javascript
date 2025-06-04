a5cb5d16c1285849cb101cd990ec14a9e2ffb6f8
Lucas Paim <lucaspaim924@gmail.com>
2025-06-03 21:57:03 -0300

    StopPropagation e desafio Ricardo

--- aula-37-stopPropagation/teste1.js
+++ aula-37-stopPropagation/teste1.js
@@ -1,10 +1,17 @@
 const caixa1 = document.querySelector("#caixa1")
 const btn_c1 = document.querySelector("#c1")
+const cursos = [...document.querySelectorAll(".cursos")]
 
 caixa1.addEventListener("click", (evento) => {
-    console.log(evento)
+    console.log("clicou")
+})
+
+cursos.map((el)=>{
+    el.addEventListener("click", (evento) => {
+        evento.stopPropagation()        //não me retorna o evento, pois coloquei stoppropagation
+    })
 })
 
 // btn_c1.addEventListener("click", (evento) => {
-//     console.log("Clicou")
+//     evento.stopPropagation()    //não me retorna evento, por conta do stopPropagation
 // })
